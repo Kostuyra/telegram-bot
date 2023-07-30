56ac6d967e19ef1b77d087427a788e1154807275
Константин <gkv110592@yandex.ru>
2023-07-30 16:48:58 +0300

    courseWork2

--- target/classes/liquibase/scripts/course-work.sql
+++ target/classes/liquibase/scripts/course-work.sql
@@ -3,7 +3,7 @@
 --changeset golikovk1: create_notification_task
 CREATE TABLE notification_task
 (
-    id SERIAL,
+    id BIGINT PRIMARY KEY,
     message TEXT NOT NULL,
     chat_id BIGINT NOT NULL,
     notification_date_time TIMESTAMP NOT NULL
